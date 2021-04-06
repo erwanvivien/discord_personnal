1da4765cf943520f41608f06f6ffbeb426be40e3
erwan.vivien <erwan.vivien@epita.fr>
2021-04-06 09:42:49 +0200

    :art: Earlier exit if message is not starting with prefix

--- src/main.py
+++ src/main.py
@@ -14,18 +14,8 @@ ERRORS = []
 DISC_LNK_DEV = "https://discord.com/api/oauth2/authorize?client_id=819549722422673448&permissions=2147544128&scope=bot%20applications.commands"
 DISC_LNK = "https://discord.com/api/oauth2/authorize?client_id=819549623172726824&permissions=2147544128&scope=bot%20applications.commands"
 
-token_file_name = "token"
+token_file_name = "token_dev"
 token = utils.get_content(token_file_name)
-
-CMDS = {
-    "!!map": commands.map,
-    "!!unmap": commands.unmap,
-    "!!mappings": commands.mappings,
-
-    "!!define": commands.define,
-
-    "!!help": commands.help,
-}
 
 
 class Client(discord.Client):
@@ -39,12 +29,16 @@ class Client(discord.Client):
             await disc.report(self, "Started", "Started successfully !")
 
     async def on_message(self, message):
-        if message.author.id in utils.BOT_IDS:        # Doesn't do anything if it's a bot message
+        author_id = message.author.id
+        if author_id in utils.BOT_IDS:        # Doesn't do anything if it's a bot message
             return
 
         split = message.content.split(' ', 1)  # separate command from args
         cmd = split[0].lower()
         args = split[1].split(' ') if len(split) > 1 else []
+
+        if not cmd.startswith("!!"):
+            return
 
         # Get Discord Nick if existant or discord Name
         name = disc.author_name(message.author, False)
