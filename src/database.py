cc66441a6600b21f6acc9b006f69e269058442a0
erwan.vivien <erwan.vivien@epita.fr>
2021-04-06 09:44:31 +0200

    :bug: Using DB_PATH now :sparkles: Started adding the needed search / insert function in DB

--- src/database.py
+++ src/database.py
@@ -1,21 +1,52 @@
 import sqlite3
 from sqlite3 import Error
 
-DB_PATH = "database.db"
+DB_PATH = "db/database.db"
 
 
 def create():
-    # Contains
-    sql_create_user = """CREATE TABLE IF NOT EXISTS guild 
+    # Contains a discord guild id
+    # and the premium remaining duration or 0 if not
+    sql_create_user = """CREATE TABLE IF NOT EXISTS guilds
     (
         id integer PRIMARY KEY,
-        premium INTEGER
+        premium REAL
     ); """
     exec(sql_create_user)
 
+    # Contains the mapping ID, pretty useless
+    # Contains the mapping name
+    # Contains the mapping path
+    # Contains the mapping definition
+    sql_create_mappings = """CREATE TABLE IF NOT EXISTS mappings
+    (
+        id integer PRIMARY KEY,
+        discord_id integer,
+        name TEXT,
+        path TEXT,
+        definition TEXT
+    ); """
+    exec(sql_create_mappings)
+
+
+def guild_exists(guild_id):
+    sql = "SELECT * FROM guilds WHERE guilds.id = ?"
+    args = [guild_id]
+
+    res = exec(sql, args)
+    return res
+
+
+def guild_insert(guild_id):
+    # Inserts the discord server in question.
+    sql = "INSERT INTO guilds VALUES (?, ?)"
+    args = [guild_id, 0]
+
+    exec(sql, args)
+
 
 def exec(sql, args=None):
-    conn = sqlite3.connect('database.db')
+    conn = sqlite3.connect(DB_PATH)
     cur = conn.cursor()
 
     if args:
